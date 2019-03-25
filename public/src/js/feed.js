aefdb2fc248abc5f97893dfef84ef242a581fd92
Roman <rombyr@outlook.com>
2019-03-24 23:56:06 -0400

    Add files via upload

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -20,15 +20,6 @@ function openCreatePostModal() {
 
     deferredPrompt = null;
   }
-
-  // if ('serviceWorker' in navigator) {
-  //   navigator.serviceWorker.getRegistrations()
-  //     .then(function(registrations) {
-  //       for (var i = 0; i < registrations.length; i++) {
-  //         registrations[i].unregister();
-  //       }
-  //     })
-  // }
 }
 
 function closeCreatePostModal() {
@@ -51,29 +42,23 @@ function onSaveButtonClicked(event) {
   }
 }
 
-function clearCards() {
-  while(sharedMomentsArea.hasChildNodes()) {
-    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
-  }
-}
-
-function createCard(data) {
+function createCard() {
   var cardWrapper = document.createElement('div');
   cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
   var cardTitle = document.createElement('div');
   cardTitle.className = 'mdl-card__title';
-  cardTitle.style.backgroundImage = 'url(' + data.image + ')';
+  cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
   cardTitle.style.backgroundSize = 'cover';
   cardTitle.style.height = '180px';
   cardWrapper.appendChild(cardTitle);
   var cardTitleTextElement = document.createElement('h2');
   cardTitleTextElement.style.color = 'white';
   cardTitleTextElement.className = 'mdl-card__title-text';
-  cardTitleTextElement.textContent = data.title;
+  cardTitleTextElement.textContent = 'San Francisco Trip';
   cardTitle.appendChild(cardTitleTextElement);
   var cardSupportingText = document.createElement('div');
   cardSupportingText.className = 'mdl-card__supporting-text';
-  cardSupportingText.textContent = data.location;
+  cardSupportingText.textContent = 'In San Francisco';
   cardSupportingText.style.textAlign = 'center';
   // var cardSaveButton = document.createElement('button');
   // cardSaveButton.textContent = 'Save';
@@ -84,45 +69,10 @@ function createCard(data) {
   sharedMomentsArea.appendChild(cardWrapper);
 }
 
-function updateUI(data) {
-  clearCards();
-  for (var i = 0; i < data.length; i++) {
-    createCard(data[i]);
-  }
-}
-
-var url = 'https://pwagram-99adf.firebaseio.com/posts.json';
-var networkDataReceived = false;
-
-fetch(url)
+fetch('https://httpbin.org/get')
   .then(function(res) {
     return res.json();
   })
   .then(function(data) {
-    networkDataReceived = true;
-    console.log('From web', data);
-    var dataArray = [];
-    for (var key in data) {
-      dataArray.push(data[key]);
-    }
-    updateUI(dataArray);
+    createCard();
   });
-
-if ('caches' in window) {
-  caches.match(url)
-    .then(function(response) {
-      if (response) {
-        return response.json();
-      }
-    })
-    .then(function(data) {
-      console.log('From cache', data);
-      if (!networkDataReceived) {
-        var dataArray = [];
-        for (var key in data) {
-          dataArray.push(data[key]);
-        }
-        updateUI(dataArray)
-      }
-    });
-}
